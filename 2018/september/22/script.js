5c4bee58fa2fc1c2c1ac0d4410068ce033c5a193
strayChris <chsalus@gmail.com>
2018-09-22 17:05:47 +0300

    Added some .js

---
+++ 2018/september/22/script.js
@@ -0,0 +1,11 @@
+let bottomB = document.getElementById('subscribe');
+let second = document.getElementById('seconda');
+let secondNew = "./new.jpg"
+
+bottomB.onclick = () => {
+	 alert('si scherza eh! su Marte ci andiamo solo in VR per ora!')
+};
+
+second.onclick = () => {
+	second.src = secondNew
+};
