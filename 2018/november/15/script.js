a0c0cca5812e30abe9689accd771ebbcdf83e304
strayChris <chsalus@gmail.com>
2018-11-15 22:50:16 +0200

    added some scripts

---
+++ 2018/november/15/script.js
@@ -0,0 +1,6 @@
+document.getElementById("now").innerHTML = '(sovra)Scritto con Java(fucking)script';
+
+let elemento = document.getElementById('now2');
+elemento.innerHTML = 'Pure questo JS, altro metodo'; /* sembra piu lungo, ma alla lunga è meglio */
+
+
