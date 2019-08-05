ba7a3d0f237d8cb88a295fed2316de27a8f784ec
dinogoja <32682971+dinogoja@users.noreply.github.com>
2019-08-05 14:23:44 +0300

    Adding 5

---
+++ 2019/aug/5/script.js
@@ -0,0 +1,6 @@
+function shit() {
+    document.getElementsByClassName("c-first")[0].style.backgroundColor = "red";
+    document.getElementsByClassName("c-second")[0].style.backgroundColor = "red";
+    document.getElementsByClassName("c-third")[0].style.backgroundColor = "red";
+  }
+  
