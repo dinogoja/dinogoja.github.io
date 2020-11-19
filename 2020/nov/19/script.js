d691b6cde0cfd544d7378788ab11915cf4c7f120
dinogoja <32682971+dinogoja@users.noreply.github.com>
2020-11-19 23:52:16 +0100

    Adding 19 nov

---
+++ 2020/nov/19/script.js
@@ -0,0 +1,8 @@
+function myFunction(x, y = 2) {
+    return x * y;
+  }
+  document.getElementById("demo").innerHTML = myFunction(5);
+
+function God() {
+  document.getElementById('demo3').style.backgroundColor='Red';
+}
